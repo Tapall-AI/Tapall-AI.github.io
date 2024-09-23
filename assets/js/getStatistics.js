0be11d15d0c797a7f378348964d263e6db4d5903
smileluck <12386904+smileluck@users.noreply.github.com>
2024-09-23 18:18:26 +0800

    feat: add num for plus char

--- assets/js/getStatistics.js
+++ assets/js/getStatistics.js
@@ -7,10 +7,10 @@ $(document).ready(function(){
             console.log(result);
             const list = $('#statistics .num');
             for(let i = 0; i < list.length; i+=4) {
-                list[i].innerHTML = result.data['videoNum'];
-                list[i+1].innerHTML = result.data['materialNum'];
+                list[i].innerHTML = result.data['videoNum']+"+";
+                list[i+1].innerHTML = result.data['materialNum']+"+";
                 list[i+2].innerHTML = result.data['searchNum'];
-                list[i+3].innerHTML = result.data['userNum'];
+                list[i+3].innerHTML = result.data['userNum']+"+";
             }
         },
         error: (e) => {
